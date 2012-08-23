d7cc99012c558c8a8d1f91277fd4ebcb99c9d6c1
Jure Triglav <juretriglav@gmail.com>
2012-08-23 21:56:37 +0200

    Adding feedback methods for generic_content

--- lib/impermium/content/generic_content.rb
+++ lib/impermium/content/generic_content.rb
@@ -2,11 +2,31 @@ module Impermium
   module GenericContent
     def generic_content(content_id, user_id, content, options={}, &block)
       options.merge!(
-	:content_id => content_id,
+	      :content_id => content_id,
         :user_id => user_id,
         :content => content,
         )
       post("generic_content", options, &block)
+    end
+
+    def generic_content_analyst_feedback(analyst_id, comment_id, desired_result, options={}, &block)
+      options.merge!(
+        :analyst_id => analyst_id,
+        :content_id => comment_id,
+        :desired_result => desired_result
+        )
+      post("generic_content/analyst_feedback", options, &block)
+    end
+
+    def generic_content_user_feedback(rep_usr_id, rep_usr_type, reporter_ip, comment_id, desired_result, options={}, &block)
+      options.merge!(
+        :reporter_user_id => rep_usr_id,
+        :reporter_user_type => REPORTER_USER_TYPE_VALUES.include?(rep_usr_type) ? rep_usr_type : REPORTER_USER_TYPE_VALUES.first,
+        :reporter_ip => reporter_ip,
+        :content_id => content_id,
+        :desired_result => desired_result
+        )
+      post("generic_content/user_feedback", options, &block)
     end
   end
 end
